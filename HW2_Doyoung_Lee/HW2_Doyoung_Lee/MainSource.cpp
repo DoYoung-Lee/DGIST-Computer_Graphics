499ba2643c4a5357c700c3e53665bc01e2d02221
Doyoung Lee <37057719+DoYoung-Lee@users.noreply.github.com>
2018-10-29 21:37:52 +0900

    checkpoint_1029

--- HW2_Doyoung_Lee/HW2_Doyoung_Lee/MainSource.cpp
+++ HW2_Doyoung_Lee/HW2_Doyoung_Lee/MainSource.cpp
@@ -3,9 +3,10 @@
 
    -------------------------------- */
 
-// #include "3DObjectFunc.h"
 #include "UserInputFunc.h"
 #include "FileFunc.h"
+#include "TimerFunc.h"
+// #include "VertexFunc.h"
 
 #include <fstream>
 #include <string>
@@ -14,7 +15,7 @@
 #include <GL/glew.h>
 #include <gl/freeglut.h>
 #include <glm/glm.hpp>
-
+#include <glm/gtc/matrix_transform.hpp>
 
 #define PI 3.141592f
 
@@ -23,6 +24,7 @@ GLuint shader_program;
 GLuint vertex_array_obj;
 GLuint vertexbuffer_obj;
 GLuint colorbuffer_obj;
+GLuint indexbufer_obj;
 
 struct Object {
 	float x, y;
@@ -32,23 +34,135 @@ struct Object {
 	int n_vertices;
 };
 
-static void InitializeGlutCallbacks(void) {
+static void CreateVertexBuffer() {
+	glCreateVertexArrays(1, &vertex_array_obj);
+	glBindVertexArray(vertex_array_obj);
 
+	float vertices[]{
+		0.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+	};	
+	
+	glGenBuffers(1, &vertexbuffer_obj);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer_obj);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+	float colors[] = {
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f,
+	};
+	glGenBuffers(1, &colorbuffer_obj);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer_obj);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		0, 3, 4,
+		0, 4, 1,
+		2, 1, 3,
+		2, 3, 6,
+		2, 6, 1,
+		5, 1, 4,
+		5, 4, 6,
+		5, 6, 1,
+		7, 3, 4,
+		7, 4, 6,
+		7, 6, 3,
+	};
+
+	glGenBuffers(1, &indexbufer_obj);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbufer_obj);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
+
+static void InitAnimObjects() {
+
+}
+
+static void RenderScene2DCB() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	GLint position_loc = glGetAttribLocation(shader_program, "Position");
+	GLint color_loc = glGetAttribLocation(shader_program, "Color");
+
+	GLint x_loc, y_loc, r_loc;
+	x_loc = glGetUniformLocation(shader_program, "x");
+	y_loc = glGetUniformLocation(shader_program, "y");
+	r_loc = glGetUniformLocation(shader_program, "rot");
+
+	glUniform1f(x_loc, 0);
+	glUniform1f(y_loc, 0);
+	glUniform1f(r_loc, 0);
+
+	//// Draw background
+	// 1st attribute buffer: vertices
+	glEnableVertexAttribArray(position_loc);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer_obj);
+	glVertexAttribPointer(position_loc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	// 2nd attribute buffer: colors
+	glEnableVertexAttribArray(color_loc);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer_obj);
+	glVertexAttribPointer(color_loc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawElements(GL_TRIANGLES, 12 * 3, GL_UNSIGNED_INT, 0);
+
+	glDisableVertexAttribArray(position_loc);
+	glDisableVertexAttribArray(color_loc);
+	
+	glutSwapBuffers();
+}
+
+static void RenderScene3DCB() {
+	GLint matrix_loc = glGetUniformLocation(shader_program, "MVP");
+	glm::mat4 projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f); // glm::perspective is member of matrix_transform.hpp
+	glm::mat4 view = glm::lookAt(
+		glm::vec3(4, 3, -3),
+		glm::vec3(0, 0, 0),
+		glm::vec3(0, 1, 0)
+	);
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 MVP = projection * view * model;
+	glUniformMatrix4fv(matrix_loc, 1, GL_FALSE, &MVP[0][0]);
+
+}
+
+static void InitGlutCallbacks(void) {
+	glutDisplayFunc(RenderScene3DCB);
+	glutKeyboardFunc(KeyboardCB);
+	glutSpecialFunc(SpecialCB);
+	glutSpecialUpFunc(SpecialUpCB);
+	glutTimerFunc(33, TimerCallBack, 0);
 }
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(768, 768);
+	glutInitWindowSize(640, 480);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("CrossRoad3D");
-	InitializeGlutCallbacks();
+	InitGlutCallbacks(); // Initialize callback functions
 	GLenum res = glewInit();
 	if (res != GLEW_OK) {
 		std::cerr << "Error: " << glewGetErrorString(res) << std::endl;
 		return 1;
 	}
-	InitShaders(shader_program, "vertex_shader.glsl", "frag_shader.glsl");
+	InitShaders(shader_program, "vertex_shader_2d.glsl", "frag_shader_2d.glsl");
+	CreateVertexBuffer(); // Create vertex buffer using vertice data
+	InitAnimObjects(); // Initialize animation objects
+	glutMainLoop();
 
 	return 0;
 }
---
+++ HW2_Doyoung_Lee/HW2_Doyoung_Lee/VertexFunc.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+/*
+std::vector<float> CubeVertices(float vertex_origin, float len_x, float len_y, float len_z) {
+
+}
+
+std::vector<float[3]> TileVertices(float vertex_origin, float len_x, float len_y) {
+	std::vector<float> vertices_temp;
+	vertices_temp.push_back((0.0f, 0.0f, 0.0f));
+
+}
+*/
